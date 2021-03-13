90da28368ee980f1159ce53996464013f557bb1d
aaronactivate <aaron@activate.co.uk>
2021-03-13 16:12:26 +0000

    add firebase configs

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  // INSERT YOUR OWN CONFIG HERE
+  apiKey: "AIzaSyCBz3mq7bVrU88lTdgnGuqKN51hq5MyVVk",
+  authDomain: "blogapp-c9c07.firebaseapp.com",
+  projectId: "blogapp-c9c07",
+  storageBucket: "blogapp-c9c07.appspot.com",
+  messagingSenderId: "640286703073",
+  appId: "1:640286703073:web:14327125318fb9330626a0"
 };
 
 try {
