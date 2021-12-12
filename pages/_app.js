d33bb05b36f3ad364f00bbca1bf785f93471cb11
aaronactivate <aaron@activate.co.uk>
2021-12-12 22:03:37 +0000

    add logrocket

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import { useEffect } from 'react';
 import fire from '../config/fire-config';
 import Head from 'next/head'
-import '../public/styles/global.scss'
+import '../public/styles/global.scss';
+import LogRocket from 'logrocket';
 
 export default function App({ Component, pageProps }) {
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
       fire.analytics();
+      LogRocket.init('qbfdxd/vitaelyme', {
+        dom: {
+          inputSanitizer: true,
+        },
+      });
     }
+
   }, [])
 
   return (
