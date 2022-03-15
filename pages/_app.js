10d6b696a30ea69fbdae4571ec328ed71aefb122
aaronactivate <aaron@activate.co.uk>
2022-03-15 20:02:51 +0000

    add crisp chat

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,18 @@ export default function App({ Component, pageProps }) {
         },
       });
     }
+    // Include the Crisp code here, without the <script></script> tags
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "02962a25-3eb1-43dd-a8bc-815253fa7582";
+
+    (function() {
+      var d = document;
+      var s = d.createElement("script");
+
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
   }, [])
 
   return (
