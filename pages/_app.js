2801274b9655b9ffa17043cf44a85441ccde6ecc
aaronactivate <aaron@activate.co.uk>
2022-03-15 20:11:36 +0000

    show chat on production only

--- pages/_app.js
+++ pages/_app.js
@@ -15,19 +15,19 @@ export default function App({ Component, pageProps }) {
           inputSanitizer: true,
         },
       });
+      // Include the Crisp code here, without the <script></script> tags
+      window.$crisp = [];
+      window.CRISP_WEBSITE_ID = "02962a25-3eb1-43dd-a8bc-815253fa7582";
+
+      (function() {
+        var d = document;
+        var s = d.createElement("script");
+
+        s.src = "https://client.crisp.chat/l.js";
+        s.async = 1;
+        d.getElementsByTagName("head")[0].appendChild(s);
+      })();
     }
-    // Include the Crisp code here, without the <script></script> tags
-    window.$crisp = [];
-    window.CRISP_WEBSITE_ID = "02962a25-3eb1-43dd-a8bc-815253fa7582";
-
-    (function() {
-      var d = document;
-      var s = d.createElement("script");
-
-      s.src = "https://client.crisp.chat/l.js";
-      s.async = 1;
-      d.getElementsByTagName("head")[0].appendChild(s);
-    })();
   }, [])
 
   return (
