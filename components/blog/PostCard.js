1c62b21ad02d2964bd4278b15ed01fd384f58b05
Aaron Butler <butler952@gmail.com>
2022-04-29 11:24:44 +0100

    wrap blog post in anchor tag

--- components/blog/PostCard.js
+++ components/blog/PostCard.js
@@ -6,22 +6,24 @@ const PostCard = (props) => {
   return (
     <>
       <Link href={props.postUrl}>
-        <div className={`d-flex flex-column flex-lg-row ${props.image ? 'align-items-center' : 'align-items-start'} bg-light-900 radius-3 p-4 p-sm-5 ${styles.postCard}`}>
-          { props.image ? (
-            <div className={`${styles.featureCard} order-1 order-lg-2 w-100`} style={{height: 'fitContent'}}>
-              <img alt={props.title} src={props.image}></img>
+        <a>
+          <div className={`d-flex flex-column flex-lg-row ${props.image ? 'align-items-center' : 'align-items-start'} bg-light-900 radius-3 p-4 p-sm-5 ${styles.postCard}`}>
+            { props.image ? (
+              <div className={`${styles.featureCard} order-1 order-lg-2 w-100`} style={{height: 'fitContent'}}>
+                <img alt={props.title} src={props.image}></img>
+              </div>
+            ) : null }
+            <div className="order-2 order-lg-1 w-100">
+              {props.screenWidth > 767 ?
+                <h3>{props.title}</h3>
+                :
+                <h3>{props.title}</h3>
+              }
+              <p className="font-weight-medium text-dark-low">{props.duration} min read</p>
+              <p className="mb-0">{props.bodyPreview} <Link href={props.postUrl} className="text-dark-low"><u style={{cursor: 'pointer'}}>Continue reading</u></Link></p>
             </div>
-          ) : null }
-          <div className="order-2 order-lg-1 w-100">
-            {props.screenWidth > 767 ?
-              <h3>{props.title}</h3>
-              :
-              <h3>{props.title}</h3>
-            }
-            <p className="font-weight-medium text-dark-low">{props.duration} min read</p>
-            <p className="mb-0">{props.bodyPreview} <Link href={props.postUrl} className="text-dark-low"><u style={{cursor: 'pointer'}}>Continue reading</u></Link></p>
           </div>
-        </div>
+        </a>
       </Link>
     </>
   )
