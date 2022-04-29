52d5151b928c529ad98106a18dbd624c27fa6ea3
Aaron Butler <butler952@gmail.com>
2022-04-29 11:32:11 +0100

    fix hover state of blog post links

--- components/blog/PostCard.js
+++ components/blog/PostCard.js
@@ -6,7 +6,7 @@ const PostCard = (props) => {
   return (
     <>
       <Link href={props.postUrl}>
-        <a>
+        <a style={{ textDecoration: 'none' }}>
           <div className={`d-flex flex-column flex-lg-row ${props.image ? 'align-items-center' : 'align-items-start'} bg-light-900 radius-3 p-4 p-sm-5 ${styles.postCard}`}>
             { props.image ? (
               <div className={`${styles.featureCard} order-1 order-lg-2 w-100`} style={{height: 'fitContent'}}>
@@ -20,7 +20,7 @@ const PostCard = (props) => {
                 <h3>{props.title}</h3>
               }
               <p className="font-weight-medium text-dark-low">{props.duration} min read</p>
-              <p className="mb-0">{props.bodyPreview} <Link href={props.postUrl} className="text-dark-low"><u style={{cursor: 'pointer'}}>Continue reading</u></Link></p>
+              <p className="text-dark-med mb-0">{props.bodyPreview} <Link href={props.postUrl} className="text-dark-low"><u style={{cursor: 'pointer'}}>Continue reading</u></Link></p>
             </div>
           </div>
         </a>
