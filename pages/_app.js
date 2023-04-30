e92a82b749180f6d03a32b6c33d76e01c150ac7a
Aaron Butler <butler952@gmail.com>
2023-04-30 23:49:37 +0100

    hide chat

--- pages/_app.js
+++ pages/_app.js
@@ -20,18 +20,18 @@ export default function App({ Component, pageProps }) {
           inputSanitizer: true,
         },
       });
-      // Include the Crisp code here, without the <script></script> tags
-      window.$crisp = [];
-      window.CRISP_WEBSITE_ID = "02962a25-3eb1-43dd-a8bc-815253fa7582";
+      // // Include the Crisp code here, without the <script></script> tags
+      // window.$crisp = [];
+      // window.CRISP_WEBSITE_ID = "02962a25-3eb1-43dd-a8bc-815253fa7582";
 
-      (function() {
-        var d = document;
-        var s = d.createElement("script");
+      // (function() {
+      //   var d = document;
+      //   var s = d.createElement("script");
 
-        s.src = "https://client.crisp.chat/l.js";
-        s.async = 1;
-        d.getElementsByTagName("head")[0].appendChild(s);
-      })();
+      //   s.src = "https://client.crisp.chat/l.js";
+      //   s.async = 1;
+      //   d.getElementsByTagName("head")[0].appendChild(s);
+      // })();
     }
   }, [])
 
