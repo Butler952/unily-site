07051b22f6156eeee9f571ba30990e85aa88d85f
Butler952 <butler952@gmail.com>
2023-06-19 19:35:39 +0100

    hide middleware

--- middleware.js
+++ middleware.js
@@ -1,8 +1,41 @@
+// // import { NextResponse } from "next/server";
+
+// // export const config = 
+// // {
+// //   matcher: [    
+// //     "/",
+// //     "/:path",
+// //     "/site/:id",
+// //     "/site/:id/:path",
+// //     "/post/:id",
+// //     "/post/:id/:path",
+// //   ],
+// // };
+
+// // export default function middleware(req) {
+
+// //   const url = req.nextUrl;
+  
+// //   const hostname = req.headers.get("host");
+
+// //   const currentHost = hostname.replace(`.localhost:3000`, "");
+
+// // //   if (url.pathname.startsWith('/_next') || url.pathname.startsWith('/favicon.ico')) {
+// // //     return NextResponse.next()
+// // // }
+// //   if (!url.pathname.includes(".") && !url.pathname.startsWith("/api")) {
+// //     url.pathname = `/_sites/${currentHost}${url.pathname}`;
+// //     return NextResponse.rewrite(url);
+// //   }
+
+// // }
+
+// // ---
+
 // import { NextResponse } from "next/server";
 
-// export const config = 
-// {
-//   matcher: [    
+// export const config = {
+//   matcher: [
 //     "/",
 //     "/:path",
 //     "/site/:id",
@@ -13,86 +46,53 @@
 // };
 
 // export default function middleware(req) {
-
 //   const url = req.nextUrl;
-  
-//   const hostname = req.headers.get("host");
 
-//   const currentHost = hostname.replace(`.localhost:3000`, "");
+//   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+//   const hostname = req.headers.get("host") || "expertpage.io";
 
-// //   if (url.pathname.startsWith('/_next') || url.pathname.startsWith('/favicon.ico')) {
-// //     return NextResponse.next()
-// // }
+//   // does this change anything????
+
+//   // // Only for demo purposes – remove this if you want to use your root domain as the landing page
+//   // if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
+//   //   return NextResponse.redirect("https://demo.vercel.pub");
+//   // }
+
+//   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+//       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+//       in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
+//       still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
+
+//   const currentHost =
+//     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+//       ? hostname
+//           .replace(`.expertpage.io`, "")
+//       : hostname.replace(`.localhost:3000`, "");
+
 //   if (!url.pathname.includes(".") && !url.pathname.startsWith("/api")) {
+//     // if (currentHost == "app") {
+//     //   if (
+//     //     url.pathname === "/login" &&
+//     //     (req.cookies.get("next-auth.session-token") ||
+//     //       req.cookies.get("__Secure-next-auth.session-token"))
+//     //   ) {
+//     //     url.pathname = "/";
+//     //     return NextResponse.redirect(url);
+//     //   }
+
+//     //   url.pathname = `/app${url.pathname}`;
+//     //   return NextResponse.rewrite(url);
+//     // }
+
+//     if (
+//       hostname === "localhost:3000" ||
+//       hostname === "expertpage.io"
+//     ) {
+//       url.pathname = `/${url.pathname}`;
+//       return NextResponse.rewrite(url);
+//     }
+
 //     url.pathname = `/_sites/${currentHost}${url.pathname}`;
 //     return NextResponse.rewrite(url);
 //   }
-
 // }
-
-// ---
-
-import { NextResponse } from "next/server";
-
-export const config = {
-  matcher: [
-    "/",
-    "/:path",
-    "/site/:id",
-    "/site/:id/:path",
-    "/post/:id",
-    "/post/:id/:path",
-  ],
-};
-
-export default function middleware(req) {
-  const url = req.nextUrl;
-
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get("host") || "expertpage.io";
-
-  // does this change anything????
-
-  // // Only for demo purposes – remove this if you want to use your root domain as the landing page
-  // if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-  //   return NextResponse.redirect("https://demo.vercel.pub");
-  // }
-
-  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
-      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
-      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
-
-  const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname
-          .replace(`.expertpage.io`, "")
-      : hostname.replace(`.localhost:3000`, "");
-
-  if (!url.pathname.includes(".") && !url.pathname.startsWith("/api")) {
-    // if (currentHost == "app") {
-    //   if (
-    //     url.pathname === "/login" &&
-    //     (req.cookies.get("next-auth.session-token") ||
-    //       req.cookies.get("__Secure-next-auth.session-token"))
-    //   ) {
-    //     url.pathname = "/";
-    //     return NextResponse.redirect(url);
-    //   }
-
-    //   url.pathname = `/app${url.pathname}`;
-    //   return NextResponse.rewrite(url);
-    // }
-
-    if (
-      hostname === "localhost:3000" ||
-      hostname === "expertpage.io"
-    ) {
-      url.pathname = `/${url.pathname}`;
-      return NextResponse.rewrite(url);
-    }
-
-    url.pathname = `/_sites/${currentHost}${url.pathname}`;
-    return NextResponse.rewrite(url);
-  }
-}
