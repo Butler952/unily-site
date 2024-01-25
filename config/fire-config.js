73cef861d661e6bbf082cfc48a4f017103a4fee7
Butler952 <butler952@gmail.com>
2024-01-25 19:05:07 +0000

    update env variables

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,26 +1,27 @@
 import firebase from 'firebase';
 
-// var firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-// };
+var firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 // Dev
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyBOUU9jkhPTraVXBNAtZDvWaIm_p5LgMhk",
-    authDomain: "vitaely-dev.firebaseapp.com",
-    projectId: "vitaely-dev",
-    storageBucket: "vitaely-dev.appspot.com",
-    messagingSenderId: "825327873857",
-    appId: "1:825327873857:web:fb7819ef359237bf30fbc0",
-    measurementId: "G-HGYSFW8SCK"
-  };
+//  var firebaseConfig = {
+//     apiKey: "AIzaSyBOUU9jkhPTraVXBNAtZDvWaIm_p5LgMhk",
+//     authDomain: "vitaely-dev.firebaseapp.com",
+//     projectId: "vitaely-dev",
+//     storageBucket: "vitaely-dev.appspot.com",
+//     messagingSenderId: "825327873857",
+//     appId: "1:825327873857:web:fb7819ef359237bf30fbc0",
+//     measurementId: "G-HGYSFW8SCK"
+//   };
+
 
 // Production
 /*
