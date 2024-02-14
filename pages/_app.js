f6c705aee37f4c28156dcc773cd921fece0d98b1
Butler952 <butler952@gmail.com>
2024-02-14 12:15:35 +0000

    attempted meta fix

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,15 @@ export default function App({ Component, pageProps }) {
         <Head>
           <title>vitaely.me</title>
           <link rel="shortcut icon" href="/images/vitaely-logo-icon-square.svg" />
+          <title>Vitaely | Turn your Linkedin profile into a website</title>
+          <meta name="description" content="Turn your Linkedin profile into your own personal website in two minutes" />
+          <meta property="og:title" content="Vitaely | Turn your Linkedin profile into a website" />
+          <meta property="og:description" content="Turn your Linkedin profile into your own personal website in two minutes" />
+          <meta property="og:url" content="https://www.vitaely.me/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://api.apiflash.com/v1/urltoimage?access_key=c0862ed5113840318341823ac08fe465&wait_until=page_loaded&url=https%3A%2F%2Fwww.vitaely.me" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content="https://api.apiflash.com/v1/urltoimage?access_key=c0862ed5113840318341823ac08fe465&wait_until=page_loaded&url=https%3A%2F%2Fwww.vitaely.me" />
         </Head>
         <Layout>
           <Component {...pageProps} />
