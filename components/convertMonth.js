dbf280f4a3c0063ac21f49eb8c64ddc690d5ef64
Butler952 <butler952@gmail.com>
2024-11-03 17:17:59 +0000

    remove a bunch of stuff

--- components/convertMonth.js
+++
@@ -1,5 +0,0 @@
-const convertMonth = (mon) => {
-  return [null, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][mon];
-}
-
-export default convertMonth;
