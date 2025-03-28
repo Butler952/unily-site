61df4acba75a1cf824122a572e766cc7e7480099
Butler952 <butler952@gmail.com>
2025-03-28 14:04:24 +0000

    remove stuff

--- pages/settings/components/manageButton.js
+++
@@ -1,7 +0,0 @@
-const ManageButton = (props) => {
-  return (
-    <button type="button" className="btn primary small medium w-100 mt-4" onClick={() => props.handleUpdate(event)}>Manage</button>
-  )
-}
-
-export default ManageButton
--- pages/settings/components/upgradeButton.js
+++
@@ -1,7 +0,0 @@
-const UpgradeButton = (props) => {
-  return (
-    <button type="button" className="btn primary small high w-100 mt-4" onClick={() => props.handleUpgrade(event)}>Upgrade</button>
-  )
-}
-
-export default UpgradeButton
